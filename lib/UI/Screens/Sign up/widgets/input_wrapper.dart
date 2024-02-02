490358b862b8ebc55cacfafe1714f3bbba2fee17
ayafathy1 <aya2.fathy123@gmail.com>
2024-02-02 20:28:23 +0200

    Update input_wrapper.dart

--- lib/UI/Screens/Sign up/widgets/input_wrapper.dart
+++ lib/UI/Screens/Sign up/widgets/input_wrapper.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:movieapp/UI/Screens/Sign%20up/controller/sign_up_controller.dart';
+import 'package:movieapp/Widgets/custom_elevated_button_widget.dart';
 import '../../../../Widgets/custom_button_widget.dart';
 import 'Input_field.dart';
 import 'button.dart';
@@ -6,34 +9,46 @@ import 'button.dart';
 class InputWrapper extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Padding(
-        padding: EdgeInsets.all(15),
-        child: SingleChildScrollView(
-            child: Column(
-              children: <Widget>[
-                SizedBox(height: 10,),
-                Container(
+    return GetBuilder(
+        init: SignUpController(),
+        builder: ( SignUpController controller ){
+      return
+        Padding(
+          padding: EdgeInsets.all(15),
+          child: SingleChildScrollView(
+          child: Column(
+          children: <Widget>[
+          SizedBox(height: 10,),
+    Container(
 
-                  decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(10)
-                  ),
-                  child: InputField(),
+    decoration: BoxDecoration(
+    color: Colors.white,
+    borderRadius: BorderRadius.circular(10)
+    ),
+    child: InputField(),
+    ),
+    SizedBox(height: 5,),
+    CustomElevatedButtonWidget(text: "Sign Up", textColor: Colors.black, onPressed:(){
+    if (controller.formkey.currentState!.validate()) {
+    controller.signUp();
+    };},
+
+
+
+    ),
+            Center(
+              child: TextButton(
+                onPressed: () {},
+                child: Text(
+                  'Already have an account? sign in',
+                  style: TextStyle(fontSize: 15,color: Colors.black),
                 ),
-                SizedBox(height: 5,),
-                Custombutton(),
-                Center(
-                    child: TextButton(
-                      onPressed: () {},
-                      child: Text(
-                        'Already have an account? sign in',
-                        style: TextStyle(fontSize: 15,color: Colors.black),
-                      ),
-                    )),
-
-              ],
-            ),
-            ),
-        );
+              ),
+            ),    ],
+    ),
+    ),
+    );
+    });
     }
-}
+  }
+
