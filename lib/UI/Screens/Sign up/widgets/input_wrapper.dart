4aaa237728a6311aee98fc53089fd4c24bb970bd
salmaa20 <salmasaid2078@gmail.com>
2024-02-04 17:49:08 +0200

    one update

--- lib/UI/Screens/Sign up/widgets/input_wrapper.dart
+++ lib/UI/Screens/Sign up/widgets/input_wrapper.dart
@@ -10,44 +10,49 @@ class InputWrapper extends StatelessWidget {
   Widget build(BuildContext context) {
     return GetBuilder(
         init: SignUpController(),
-        builder: ( SignUpController controller ){
-      return
-        Padding(
-          padding: EdgeInsets.all(15),
-          child: SingleChildScrollView(
-          child: Column(
-          children: <Widget>[
-          SizedBox(height: 5,),
-    Container(
+        builder: (SignUpController controller) {
+          return Padding(
+            padding: EdgeInsets.all(15),
+            child: SingleChildScrollView(
+              child: Column(
+                children: <Widget>[
+                  SizedBox(
+                    height: 5,
+                  ),
+                  Container(
+                    decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(10)),
+                    child: InputField(),
+                  ),
+                  SizedBox(
+                    height: 5,
+                  ),
+                  CustomElevatedButtonWidget(
+                    text: "Sign Up",
+                    textColor: Colors.black,
+                    onPressed: () {
 
-    decoration: BoxDecoration(
-    color: Colors.white,
-    borderRadius: BorderRadius.circular(10)
-    ),
-    child: InputField(),
-    ),
-    SizedBox(height: 5,),
+                      if (controller.formkey.currentState!.validate()) {
+                        controller.signUp();
+                        Get.to(ProfileScreen());
+                      }
 
-     CustomElevatedButtonWidget(text: "Sign Up", textColor: Colors.black,
-       onPressed:(){Get.to(ProfileScreen());
-    if (controller.formkey.currentState!.validate()) {
-    controller.signUp();
-    };},
-
-    ),
-            Center(
-              child: TextButton(
-                onPressed: () {},
-                child: Text(
-                  'Already have an account? sign in',
-                  style: TextStyle(fontSize: 15,color: Colors.black),
-                ),
+                    },
+                  ),
+                  Center(
+                    child: TextButton(
+                      onPressed: () {},
+                      child: Text(
+                        'Already have an account? sign in',
+                        style: TextStyle(fontSize: 15, color: Colors.black),
+                      ),
+                    ),
+                  ),
+                ],
               ),
-            ),    ],
-    ),
-    ),
-    );
-    });
-    }
+            ),
+          );
+        });
   }
-
+}
